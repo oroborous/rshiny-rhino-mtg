963866169a26b0c7c4653400a5b2e27b8ee1311b
Stacy Read <sread@ivaalsolutions.com>
2023-04-29 10:08:45 -0500

    Routing with nav bar

--- app/main.R
+++ app/main.R
@@ -1,5 +1,6 @@
 box::use(
-  shiny[bootstrapPage, moduleServer, NS, reactive, titlePanel, div],
+  shiny[bootstrapPage, moduleServer, NS, reactive, titlePanel, div, tags, a],
+  shiny.router[router_ui, router_server, route, route_link]
 )
 box::use(
   app/view[table, chart],
@@ -12,18 +13,39 @@ card <- function(...) div(class="card", ...)
 #' @export
 ui <- function(id) {
   ns <- NS(id)
+
   bootstrapPage(
-    titlePanel("Rhino population over time"),
-    grid(
-      card(chart$ui(ns("chart"))),
-      card(table$ui(ns("table")))
+    tags$nav(
+      class="navbar",
+      tags$ul(
+        class="nav navbar-nav",
+        tags$li(
+          a("Table", href=route_link("table"))
+        ),
+        tags$li(
+          a("Chart", href=route_link("chart"))
+        )
+      )
+    ),
+    router_ui(
+      route("table", table$ui(ns("table"))),
+      route("chart", chart$ui(ns("chart")))
     )
   )
+  #bootstrapPage(
+  #  titlePanel("Rhino population over time"),
+  #  grid(
+  #    card(chart$ui(ns("chart"))),
+  #    card(table$ui(ns("table")))
+  #  )
+  #)
 }
 
 #' @export
 server <- function(id) {
   moduleServer(id, function(input, output, session) {
+    router_server("table")
+
     data <- reactive(rhinos$fetch_data())
     chart$server("chart", data)
     table$server("table", data)
